e37d3b8dbdbab920569518aa088b8eb2a33e644e
Taui Silva Lima <tauisilva@gmail.com>
2024-11-08 20:09:11 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { InfosComponent } from "./infos/infos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfosComponent, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'techhub-app';
+  items = [
+    { name: 'Home', icon: 'bi-house', route: 'home' },
+    { name: 'Sobre', icon: 'bi-info-square', route: 'about' },
+  ]
+  constructor(
+    private router: Router
+  ) { }
+
+  navigate(r: string) {
+    this.router.navigate([r]);
+  }
 }
