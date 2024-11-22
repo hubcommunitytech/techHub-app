e897daf71331389b17d2dfc1cd0c3ee47ba84afa
Taui Silva <tauisilva@gmail.com>
2024-11-21 22:00:54 -0300

    db

--- server/js/db_mongo.js
+++ server/js/db_mongo.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
-const uri = 'mongodb+srv://hub_db:hub_db@techhub-db.doja6.mongodb.net/?retryWrites=true&w=majority&appName=techhub-db';
+const uri = process.env.MONGO_URI || 'mongodb+srv://hub_db:hub_db@techhub-db.doja6.mongodb.net/techhub?retryWrites=true&w=majority';
 // Função para conectar ao MongoDB
 const client = new MongoClient(uri, {
   serverApi: {
@@ -14,13 +14,13 @@ const client = new MongoClient(uri, {
 
 async function connect() {
   try {
-    // Conectar ao MongoDB
     await client.connect();
     console.log("Conectado ao MongoDB com sucesso!");
     const db = client.db("techhub");
     return db;
   } catch (error) {
-    console.error("Erro ao conectar ao MongoDB", error);
+    console.error("Erro ao conectar ao MongoDB:", error.message);
+    console.error("Detalhes:", error);
   }
 }
 
