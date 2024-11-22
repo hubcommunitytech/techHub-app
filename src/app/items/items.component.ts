e029c94081a921ccc4dd611901f132cb1097e5b1
Taui Silva <tauisilva@gmail.com>
2024-11-22 01:00:06 -0300

    skeleton

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -3,24 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DialogService } from 'primeng/dynamicdialog';
 import { PaginatorModule } from 'primeng/paginator';
+import { SkeletonModule } from 'primeng/skeleton';
 import { Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { ModalViewComponent } from '../util/modal-view.component';
 import { Count, Project } from './items';
 import { ItemsService } from './items.service';
-
-interface PageEvent {
-  first: number;
-  rows: number;
-  page: number;
-  pageCount: number;
-}
-
 @Component({
   selector: 'app-items',
   standalone: true,
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss'],
-  imports: [PaginatorModule, NgStyle, FormsModule, NgIf, NgFor],
+  imports: [PaginatorModule, NgStyle, FormsModule,
+    NgIf, NgFor, SkeletonModule
+  ],
   viewProviders: [DialogService]
 })
 export class ItemsComponent implements OnInit {
@@ -31,7 +26,8 @@ export class ItemsComponent implements OnInit {
   rows: number = 5;
   rowsPerPageOptions = [5, 10, 25, 50];
   searchValue: string = '';
-  private searchSubject = new Subject<string>();  // Usado para o debounce
+  private searchSubject = new Subject<string>();
+  skeleton: boolean = true;
 
   constructor(
     private service: ItemsService,
@@ -39,34 +35,36 @@ export class ItemsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // Inicia a contagem total de itens
     this.service.getTotalItems().subscribe((data: Count) => {
       this.totalRecords = data.total;
       this.loadItems();
     });
 
-    // Observa o termo de busca com debounce para otimizar as requisições
     this.searchSubject.pipe(
-      debounceTime(500),  // Espera 500ms após o último caractere
-      distinctUntilChanged(),  // Evita chamadas repetidas para o mesmo termo
+      debounceTime(500),
+      distinctUntilChanged(),
       switchMap(query => {
+        this.skeleton = true;
         return this.service.searchItems(query, this.first, this.rows);
       })
     ).subscribe((data: Project[]) => {
       this.items = data;
-      this.filteredItems = data;  // Filtra os itens com base no retorno da API
+      this.filteredItems = data;
+      this.skeleton = false;
     });
   }
 
-  // Chama a API de busca ao digitar no campo de busca
+
   onSearchChange() {
-    this.searchSubject.next(this.searchValue);  // Envia o valor da busca para o observable
+    this.searchSubject.next(this.searchValue);
   }
 
   loadItems(): void {
+    this.skeleton = true;
     this.service.searchItems(this.searchValue, this.first, this.rows).subscribe((data: Project[]) => {
       this.items = data;
       this.filteredItems = [...this.items];
+      this.skeleton = false;
     });
   }
 
